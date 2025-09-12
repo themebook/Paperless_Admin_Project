192388e049bf02032818be83f63b4c61396785fe
themebook <wbean31@naver.com>
2025-09-12 11:58:54 +0900

    init

---
+++ src/test/java/kd/Paperless_Admin_Project/PaperlessAdminProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kd.Paperless_Admin_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessAdminProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
