9da277a0792dc717b297a3addbdd6d2ad1b3692f
themebook <wbean31@naver.com>
2025-10-22 15:36:37 +0900

    공지사항 수정시 첨부파일 안 들어가는 것 수정, 사이드바 액티브 수정

--- src/main/java/kd/Paperless_Admin_Project/dto/notice/NoticeUpdateDto.java
+++ src/main/java/kd/Paperless_Admin_Project/dto/notice/NoticeUpdateDto.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import kd.Paperless_Admin_Project.entity.notice.Notice;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -27,6 +30,8 @@ public class NoticeUpdateDto {
   @Builder.Default
   @Pattern(regexp = "^(ADMIN|USER)$", message = "대상은 ADMIN 또는 USER 입니다.")
   private String targetAudience = "ADMIN";
+
+  private List<MultipartFile> files;
 
   public static NoticeUpdateDto fromEntity(Notice e) {
     if (e == null)
